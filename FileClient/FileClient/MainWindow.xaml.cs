5eca85b832db1a58d95ab0764f6977206e941c1b
Yukky <egorka0303@inbox.ru>
2025-05-03 04:33:50 +0300

    Initial commit: Server, Client, and Mobile scaffold

---
+++ FileClient/FileClient/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace FileClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient _client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client.BaseAddress = new Uri("https://localhost:7012");
+        }
+
+        private async void OnGetFileClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var file = await _client.GetFromJsonAsync<List<string>>("api/file");
+                FilesList.ItemsSource = file;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при получении файлов:\n" + ex.Message);
+            }
+        }
+
+        private async void OnUploadFileClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                var fileSream = File.OpenRead(dialog.FileName);
+                var content = new MultipartFormDataContent();
+                content.Add(new StreamContent(fileSream), "file", System.IO.Path.GetFileName(dialog.FileName));
+
+                try
+                {
+                    var response = await _client.PostAsync("api/file", content);
+                    var result = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show("Ответ сервера: " + result);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при загрузке:\n" + ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ LocalFileServer/LocalFileServer/Controllers/FileController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+
+
+namespace LocalFileServer.Controllers
+{
+    [ApiController] // Указывает, что это Web API-контроллер
+    [Route("api/[controller]")] // Адрес будет: api/file
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string path = "D:\\ProgektsVS";
+
+            if (!Directory.Exists(path))
+            {
+                return new List<string>{"Нет такой папки"};
+            }
+
+            var files = Directory.GetFiles(path);
+            return files.Select(Path.GetFileName);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Файл не передан или он пуст");
+            }
+
+            string uploadPath = "D:\\ProgektsVS";
+
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            string filePath = Path.Combine(uploadPath, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Файл " + file.FileName + " загружен!");
+        }
+    }
+}
